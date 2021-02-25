a64588b8f97eae052f5b04d29f0b767af7b05f0b
meklit <meklit_teshome@diveintocode.jp>
2021-02-26 00:17:28 +0900

    restaurant view edited

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,65 @@
 class RestaurantsController < ApplicationController
+  before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
+before_action :authenticate_user!, only: [:new, :edit]
+
+def index
+if params[:category].blank?
+@books = Restaurant.all.order("created_at DESC")
+else
+@category_id = Category.find_by(name: params[:category]).id
+@restaurants = Restaurant.where(:category_id => @category_id).order("created_at DESC")
+end
+end
+
+def show
+if @restaurant.reviews.blank?
+@average_review = 0
+else
+@average_review = @restaurant.reviews.average(:rating).round(2)
+end
+end
+
+def new
+@restaurant = current_user.restaurants.build
+@categories = Category.all.map{ |c| [c.name, c.id] }
+end
+
+def create
+@restaurant = current_user.restaurants.build(restaurant_params)
+@restaurant.category_id = params[:category_id]
+
+if @restaurant.save
+redirect_to root_path
+else
+render 'new'
+end
+end
+
+def edit
+@categories = Category.all.map{ |c| [c.name, c.id] }
+end
+
+def update
+@restaurant.category_id = params[:category_id]
+if @restaurant.update(restaurant_params)
+redirect_to restaurant_path(@restaurant)
+else
+render 'edit'
+end
+end
+
+def destroy
+@restaurant.destroy
+redirect_to root_path
+end
+
+private
+
+def restaurant_params
+params.require(:restaurant).permit(:name, :address, :specials, :category_id, :rest_image)
+end
+
+def find_restaurant
+@restaurant = Restaurant.find(params[:id])
+end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,6 +3,6 @@ class Restaurant < ApplicationRecord
   belongs_to :category
   has_many :reviews
 
- has_attached_file :rest_image, styles: { :rest_index=> "250x350>", rest_show=>: "325x475>" }, default_url: "/images/:style/missing.png"
+ has_attached_file :rest_image, styles: { :rest_index=> "250x350>", :rest_show=> "325x475>" }, default_url: "/images/:style/missing.png"
  validates_attachment_content_type :rest_image, content_type: /\Aimage\/.*\z/
 end
